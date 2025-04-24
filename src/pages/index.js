a8b2d532445ce4a294200746688d22d1d074059d
Aditya Pateriya <adityapateriya7986@gmail.com>
2025-04-24 23:10:37 +0530

    Website Updated!!

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
         <title>Portfolio | Aditya Pateriya</title>
         <meta name="description" content="Welcome to the portfolio of Aditya Pateriya." />
         
